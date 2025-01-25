8aa1a98781948bed130301dc9f401955dce03cae
Jonathan 'theJPster' Pallant <github@thejpster.org.uk>
2025-01-25 22:16:23 +0000

    Added Armv8-R start-up sequence
    If we boot at EL2, we manage to switch to EL1, however performing a 'SVC' instruction puts us in a mode where our stack pointer is NULL, crashing the CPU.
    
    The default exception handler also does semihosting now.

--- cortex-r-rt/src/lib.rs
+++ cortex-r-rt/src/lib.rs
@@ -2,6 +2,16 @@
 
 #![no_std]
 
+/// Our default exception handler.
+///
+/// We end up here if an exception fires and the weak 'PROVIDE' in the link.x
+/// file hasn't been over-ridden.
+#[no_mangle]
+pub extern "C" fn _default_handler() {
+    arm_semihosting::hprintln!("Unhandled exception!");
+    arm_semihosting::debug::exit(arm_semihosting::debug::EXIT_FAILURE);
+}
+
 // The Interrupt Vector Table
 #[cfg(any(arm_architecture = "v7-r", arm_architecture = "v8-r"))]
 core::arch::global_asm!(
@@ -11,13 +21,13 @@ core::arch::global_asm!(
 .code 32
 .align 0
 _vector_table:
-    ldr pc,=_start
-    ldr pc,=_asm_undefined_handler
-    ldr pc,=_asm_svc_handler
-    ldr pc,=_asm_prefetch_handler
-    ldr pc,=_asm_abort_handler
+    ldr pc, =_start
+    ldr pc, =_asm_undefined_handler
+    ldr pc, =_asm_svc_handler
+    ldr pc, =_asm_prefetch_handler
+    ldr pc, =_asm_abort_handler
     nop
-    ldr pc,=_asm_irq_handler
+    ldr pc, =_asm_irq_handler
 "#
 );
 
@@ -92,12 +102,6 @@ _asm_irq_handler:
     b       _irq_handler
     // restore state (the ^ means copy SPSR back to CPSR)
     ldmia    sp!, {{r0-r3, r12, pc}}^
-
-// Our default (c-compatible) handler crashes the CPU
-.global _default_handler
-_default_handler:
-    udf 0
-    b       _default_handler
 "#
 );
 
@@ -217,17 +221,14 @@ _asm_irq_handler:
     vmsr    FPSCR, r0
     vpop    {{d0-d7}}
     ldmia    sp!, {{r0-r3, r12, pc}}^
-
-// Our default (c-compatible) handler crashes the CPU
-.global _default_handler
-_default_handler:
-    udf 0
-    b       _default_handler
 "#
 );
 
+// Start-up code for Armv7-R with an FPU
+//
+// We boot into Supervisor mode, set up a stack pointer, and run `kmain` in supervisor mode.
 #[cfg(all(
-    any(arm_architecture = "v7-r", arm_architecture = "v8-r"),
+    arm_architecture = "v7-r",
     any(target_abi = "eabihf", feature = "eabi-fpu")
 ))]
 core::arch::global_asm!(
@@ -236,6 +237,7 @@ core::arch::global_asm!(
 .global _start
 .code 32
 .align 0
+.arch armv7-r
 // Work around https://github.com/rust-lang/rust/issues/127269
 .fpu vfp3-d16
 
@@ -257,8 +259,11 @@ _start:
 "#
 );
 
+// Start-up code for Armv7-R without an FPU
+//
+// We boot into Supervisor mode, set up a stack pointer, and run `kmain` in supervisor mode.
 #[cfg(all(
-    any(arm_architecture = "v7-r", arm_architecture = "v8-r"),
+    arm_architecture = "v7-r",
     not(any(target_abi = "eabihf", feature = "eabi-fpu"))
 ))]
 core::arch::global_asm!(
@@ -266,11 +271,69 @@ core::arch::global_asm!(
 .section .text.startup
 .global _start
 .code 32
+.arch armv7-r
 .align 0
 
 _start:
     // Set stack pointer
     ldr     sp, =_stack_top
+    // Jump to application
+    bl      kmain
+    // In case the application returns, loop forever
+    b       .
+"#
+);
+
+// Start-up code for Armv8-R
+//
+// We boot into EL2, set up a stack pointer, and run `kmain` in EL1.
+#[cfg(arm_architecture = "v8-r")]
+core::arch::global_asm!(
+    r#"
+.section .text.startup
+.global _start
+.code 32
+.align 0
+// Work around https://github.com/rust-lang/rust/issues/127269
+.fpu vfp3-d16
+
+_start:
+    // Are we in EL2? If not, skip the EL2 setup portion
+    mrs     r0, cpsr
+    and     r0, r0, #0x1f
+    cmp     r0, #0x1a
+    bne     1f
+    // Set stack pointer
+    ldr     sp, =_stack_top
+    // Set the HVBAR (for EL2) to _vector_table
+    ldr     r0, =_vector_table
+    mcr     p15, 4, r0, c12, c0, 0
+    // Configure HACTLR to let us enter EL1
+    mrc     p15, 4, r0, c1, c0, 1
+    orr     r0, r0, #0x7700
+    orr     r0, r0, #0x0083
+    mcr     p15, 4, r0, c1, c0, 1
+    // Program the SPSR - enter supervisor mode in Arm mode with IRQ, FIQ and Async Abort masked
+    mov		r0, #0x1DF
+	msr		spsr_hyp, r0
+	adr		r0, 1f
+	msr		elr_hyp, r0
+	dsb
+	isb
+	eret
+1:
+    // Set stack pointer (we can trash the EL2 stack - we're not coming back)
+    ldr     sp, =_stack_top
+    // Set the VBAR (for EL1) to _vector_table
+    ldr     r0, =_vector_table
+    mcr     p15, 0, r0, c12, c0, 0
+    // Allow VFP coprocessor access
+    mrc     p15, 0, r0, c1, c0, 2
+    orr     r0, r0, #0xF00000
+    mcr     p15, 0, r0, c1, c0, 2
+    // Enable VFP
+    mov     r0, #0x40000000
+    vmsr    fpexc, r0
     // Jump to application
     bl      kmain
     // In case the application returns, loop forever
